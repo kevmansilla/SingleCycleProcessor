432b08b2a7c83e4a24ed1e6d34f4cc581432ecbc
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2023-09-07 13:18:29 -0300

    ok guide 2, excercise 1

--- execute_tb.sv
+++ execute_tb.sv
@@ -1,2 +1,29 @@
-module execute_tv();
+module execute_tb();
+	parameter N=64;
+	logic AluSrc;
+   logic [3:0] AluControl;
+	logic [N-1:0] PC_E,signImm_E, readData1_E, readData2_E, PCBranch_E, aluResult_E, writeData_E;
+	logic zero_E;
+	
+	execute #(N) dut(AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E, zero_E, PCBranch_E, aluResult_E, writeData_E);
+	
+	initial begin
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b1,4'b1,64'b1,64'b1,64'b1, 64'b1};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b0,64'd5,64'b1,64'b1}) $display("error 1");
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b1,4'b0,64'b1,64'b1,64'b0, 64'b1};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b1,64'd5,64'b0,64'b1}) $display("error 2");
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b0,4'b1,64'b1,64'b1,64'b1, 64'b10};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b0,64'd5,64'b11,64'b10}) $display("error 3");
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b1,4'b1,64'b1,64'b10,64'b1, 64'b1};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b0,64'd9,64'b11,64'b1}) $display("error 4");
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b0,4'b111,64'b1,64'b1,64'b1, 64'b10};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b0,64'd5,64'b10,64'b10}) $display("error 5");
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b1,4'b10,64'b1,64'b1,64'b1, 64'b1};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b0,64'd5,64'b10,64'b1}) $display("error 6");
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b1,4'b110,64'b1,64'b1,64'b1,64'b1};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b1,64'd5,64'd0,64'b1}) $display("error 7");
+		{AluSrc, AluControl, PC_E, signImm_E, readData1_E, readData2_E} = {1'b1,4'b10,64'b1,64'b1,64'hFFFFFFFFFFFFFFFE,64'b1};#10ns;
+		if({zero_E, PCBranch_E, aluResult_E, writeData_E}!=={1'b0,64'd5,64'hFFFFFFFFFFFFFFFF,64'b1}) $display("error 8");
+	end
+
 endmodule
--- fetch_tb.sv
+++ fetch_tb.sv
@@ -1,2 +1,52 @@
 module fetch_tb();
+	logic PCSrc_F, clk, reset;
+   logic [63:0] PCBranch_F, imem_addr_F;
+	logic [4:0] error, vectornum;
+	integer test=0;
+	
+	fetch dut(PCSrc_F, clk, reset, PCBranch_F, imem_addr_F);
+	
+	always
+		begin
+			clk=0;#5ns;clk=1;#5ns;
+		end
+	
+	initial begin
+		error = 0; vectornum = 0;
+		reset=1;#50ns;reset=0;
+		PCSrc_F = 'b0;#40ns;PCSrc_F = 'b1;
+	end
+	
+	always @(negedge clk)
+		begin
+			#1;
+			PCBranch_F = 64'b101010101010;
+		end
+
+	always @(posedge clk)
+		begin
+			if (PCSrc_F) begin
+				#1;
+				if (imem_addr_F !== PCBranch_F) begin
+					error += 1;
+					$display("immem: %d", imem_addr_F);
+					$display("pc: %d", PCBranch_F);
+				end
+			end
+			else if (~PCSrc_F) begin
+				#1;if (imem_addr_F !== test+4) begin
+					error += 1;
+					$display("immem: %d", imem_addr_F);
+					$display("test: %d", test);
+				end
+				test += 4;
+			end
+			vectornum = vectornum +1;
+			if (vectornum === 15) begin 
+				$display("%d tests completed with %d errors", 
+                vectornum, error);
+				$stop;
+			end
+		end
+	
 endmodule
