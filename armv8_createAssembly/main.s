ca8453e637d3c74acf5d111e831e0d449884c6b8
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2023-09-08 11:13:33 -0300

    update

--- armv8_createAssembly/main.s
+++
@@ -1,9 +0,0 @@
-    .text
-    .org 0x0000
-
-    add x2, x0, XZR  stur x30, [x0, #240]
-loop:  stur x2, [x0, #0]
-    add x2,x2,x1  add x0,x0,x8
-    sub x30, x30, x1  cbz x30,infLoop
-    cbz XZR,loopinfLoop:  
-    cbz XZR, infLoop
